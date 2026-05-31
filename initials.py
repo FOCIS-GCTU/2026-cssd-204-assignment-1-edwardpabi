75310db0e34a251fafa786979dd4ac276d6c48fa
Edward Pabi <2425403621@live.gctu.edu.gh>
2026-05-31 21:51:57 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Edward Kwesi Pabi
+# STUDENT ID: 2425403621
+# Email: 2425403621@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Edward Kwesi Pabi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials EKP in small form, then in large 12x10 block letters.
 
+    print()
+    print("...EKP")
+    print()
+
+    print("...EEEEEEEEEEEE........KK......KK..........PPPPPPPPPP.......")
+    print("...EEEEEEEEEEEE........KK.....KK...........PPPPPPPPPPPP.....")
+    print("...EE..................KK....KK............PP........PP.....")
+    print("...EE..................KK...KK.............PP........PP.....")
+    print("...EEEEEEEEE...........KKKKKK..............PPPPPPPPPPPP.....")
+    print("...EEEEEEEEE...........KKKKKK..............PPPPPPPPPP.......")
+    print("...EE..................KK...KK.............PP...............")
+    print("...EE..................KK....KK............PP...............")
+    print("...EEEEEEEEEEEE...**...KK.....KK......**...PP.............**")
+    print("...EEEEEEEEEEEE...**...KK......KK.....**...PP.............**")
+    print()
 
 
 main()
